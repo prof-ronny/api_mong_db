921fd5611d5cdae6251b0045999a3ffb35f9aef2
Carlos Ronny de Sousa <135059087+prof-ronny@users.noreply.github.com>
2025-03-14 23:54:34 +0000

    Cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors'); 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 
 const contatoRouter = require('./routes/contatoRoutes');
 app.use('/contatos', contatoRouter);
